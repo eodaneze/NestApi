b0ba5d49399f9e51b816e373ece1a51456043259
eodaneze <ezealiokechukwu1999@gmail.com>
2024-05-19 02:19:28 -0700

    create book and get book endpoint

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body} from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+    constructor(private bookService: BookService){}
+
+    @Get()
+    async getAllBooks(): Promise<Book[]>{
+         return this.bookService.findAll()
+    }
+
+    @Post('create')
+    async createBook(@Body() book): Promise<Book>{
+        return this.bookService.create(book)
+    }
+}
